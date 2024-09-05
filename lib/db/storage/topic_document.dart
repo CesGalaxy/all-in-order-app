19a2497645da5c531b99b5242dfefd9f8158338a
CesGalaxy <cesgalaxy@outlook.com>
2024-09-05 13:54:46 +0000

    feat: :construction: Add methods for getting topic files

---
+++ lib/db/storage/topic_document.dart
@@ -0,0 +1,18 @@
+import 'dart:typed_data';
+
+import 'package:all_in_order/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<Uint8List> getTopicDocumentByName(int topicId, String name) {
+  return supabase.storage.from("topic_documents").download("$topicId/$name.md");
+}
+
+Future<List<FileObject>> getAllTopicsDocuments() {
+  return supabase.storage.from("topic_documents").list();
+}
+
+Future<List<FileObject>> getTopicDocuments(int topicId) async {
+  final allDocs = await getAllTopicsDocuments();
+
+  return allDocs.where((file) => file.name.startsWith("$topicId/")).toList();
+}
